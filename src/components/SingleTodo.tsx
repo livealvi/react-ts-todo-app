1113bf01852f97c7f1cfae82d109f94c1441a05c
Alvi Hasan <livealvi@gmail.com>
2022-05-15 23:12:48 +0600

    add: Task Delete

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -19,6 +19,10 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     );
   };
 
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <form className="todos__single">
       {todo.isDone ? (
@@ -31,7 +35,7 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
         <span className="icon" onClick={() => handleDone(todo.id)}>
